2441c121f3958d2049e85e04f1287fd0a081679b
L9DJULO <98429398+L9DJULO@users.noreply.github.com>
2023-05-23 15:13:29 +0200

    debut de banger

--- Assets/Scripts/PowerUp/PowerUpWallRun.cs
+++ Assets/Scripts/PowerUp/PowerUpWallRun.cs
@@ -7,37 +7,29 @@ public class PowerUpWallRun : MonoBehaviour
     public static bool HaveWallRun =false;
     public GameObject pickupEffect;
     public GameObject Panel;
-    public GameObject Healthbar;
-    private bool test = false;
-    private void Update()
-    {
-        if (test)
-            Healthbar.SetActive(false);
-    }
+  
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            Pickup(other);
+            StartCoroutine(_enumerator(other));
         }
-        
     }
+
+	IEnumerator _enumerator(Collider other)
+    {
+        Panel.SetActive(true);
+        yield return new WaitForSeconds(2);
+        Panel.SetActive(false);
+        Pickup(other);
+    }
+
     void Pickup(Collider Player)
     {
         // Spawn a cool effect
         Instantiate(pickupEffect, transform.position, transform.rotation);
         // Apply effect to the player
         HaveWallRun = true;
-        StartCoroutine(ReadingTime());
-    }
-
-    IEnumerator ReadingTime()
-    {
-        test = true;
-        Panel.SetActive(true);
-        yield return new WaitForSeconds(6);
-        Panel.SetActive(false);
-        Healthbar.SetActive(true); 
-        Destroy(gameObject);
+		Destroy(gameObject);
     }
 }
